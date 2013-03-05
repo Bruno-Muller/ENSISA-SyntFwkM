ee7e6266f7434ea6245c501e44ab410e39c87c7b
Bruno-Muller <muller.bru@gmail.com>
2013-03-05 17:54:40 +0100

    <<Interface>> Expression

---
+++ SyntFwkM/expression.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   expression.h
+ * Author: bruno
+ *
+ * Created on 5 mars 2013, 17:49
+ */
+
+#ifndef EXPRESSION_H
+#define	EXPRESSION_H
+
+namespace core {
+    
+    template<class T>
+    class Expression {
+        public:
+            virtual T evalutate();
+    };
+}
+
+
+#endif	/* EXPRESSION_H */
+
