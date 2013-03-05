e2af47f5b304a9a6e29b99b308fa1b8a27f4e10d
Bruno-Muller <muller.bru@gmail.com>
2013-03-05 17:39:57 +0100

    commit de test

---
+++ SyntFwkM/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: bruno
+ *
+ * Created on 5 mars 2013, 17:38
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
