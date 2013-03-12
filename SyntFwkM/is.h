292fd6e461de479d1fef78f9980b6274a9f18363
Bruno-Muller <muller.bru@gmail.com>
2013-03-12 09:43:48 +0100

    is.h

---
+++ SyntFwkM/is.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   is.h
+ * Author: bruno
+ *
+ * Created on 12 mars 2013, 09:43
+ */
+
+#ifndef IS_H
+#define	IS_H
+
+#include "expression.h"
+#include "unaryexpression.h"
+
+namespace fuzzy {
+
+    template <class T>
+    class Is : core::UnaryExpression<T>  {
+    public:
+        virtual T evaluate(const core::Expression<T>* o) const=0;
+    };  
+    
+}
+
+#endif	/* IS_H */
+
