2bca26d8a5c85a0e137a36f3b0c24f795f723e80
Bruno-Muller <muller.bru@gmail.com>
2013-03-12 08:32:13 +0100

    expressionfactory

--- SyntFwkM/expressionfactory.h
+++ SyntFwkM/expressionfactory.h
@@ -8,11 +8,35 @@
 #ifndef EXPRESSIONFACTORY_H
 #define	EXPRESSIONFACTORY_H
 
+#include "expression.h"
+#include "unaryexpression.h"
+
 namespace core {
-    
+
+    template <class T>
     class ExpressionFactory {
-        
+    public:
+        virtual Expression<T>* Hold(const Expression<T>* e) const;
+        virtual Expression<T>* NewUnary(const UnaryExpression<T>* ope, const Expression<T>* o) const;
+        virtual Expression<T>* NewBinary(const UnaryExpression<T>* ope, const Expression<T>* l, const Expression<T>* r) const;
+    private:
+        Expression<T> m_memory;
     };
+
+    template <class T>
+    Expression<T>* ExpressionFactory<T>::Hold(const Expression<T>* e) const {
+        return 0; // TODO
+    }
+
+    template <class T>
+    Expression<T>* ExpressionFactory<T>::NewUnary(const UnaryExpression<T>* ope, const Expression<T>* o) const {
+        return new UnaryExpressionModel<T>(ope, o);
+    }
+
+    template <class T>
+    Expression<T>* ExpressionFactory<T>::NewBinary(const UnaryExpression<T>* ope, const Expression<T>* l, const Expression<T>* r) const {
+        return 0; // TODO
+    }
 }
 
 
