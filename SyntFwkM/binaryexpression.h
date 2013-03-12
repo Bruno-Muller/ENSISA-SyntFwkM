07f5914c41565f2e466562c11944524376143573
CHEVALIER <s.chevaleir@iariss.fr>
2013-03-12 08:58:52 +0100

    V1 de binaryexpression & binaryexpressionmodel

---
+++ SyntFwkM/binaryexpression.h
@@ -0,0 +1,25 @@
+#ifndef BINARYEXPRESSION_H
+#define	BINARYEXPRESSION_H
+
+#include "expression.h"
+
+namespace core {
+
+    template <class T>
+    class BinaryExpression {
+    public:
+        virtual T evaluate(core::Expression<T>* left, core::Expression<T>* right) const;
+    };
+
+    template <class T>
+    T Expression<T>::evaluate(core::Expression<T>* left, core::Expression<T>* right) const;
+    {
+    	T l=left->evaluate();
+    	T r=right->evaluate();
+    }
+
+
+    
+}
+
+#endif	/* BINARYEXPRESSION_H */
---
+++ SyntFwkM/binaryexpressionmodel.h
@@ -0,0 +1,37 @@
+#ifndef BINARYEXPRESSIONMODEL_H
+#define	BINARYEXPRESSIONMODEL_H
+
+#include "expression.h"
+#include "binaryexpression.h"
+
+namespace core {
+
+    template <class T>
+    class BinaryExpressionModel : Expression<T>, BinaryExpression<T> {
+    public:
+        virtual T evalutate() const;
+        virtual T evaluate(const Expression<T>* l, const Expression<T>* r) const;
+
+    private:
+        BinaryExpression<T>* m_operator;
+        Expression<T>* m_left;
+        Expression<T>* m_right;
+    };
+
+    
+
+
+    template <class T>
+    T BinaryExpressionModel<T>::evalutate() const {
+        if (m_left != 0 && m_right != 0)
+            return evaluate(m_left,m_right);
+    }
+
+    template <class T>
+    T BinaryExpressionModel<T>::evaluate(const Expression<T>* l, const Expression<T>* r) const;
+        if (&m_operator != 0)
+            return m_operator.evaluate(l,r);
+
+}
+
+#endif
