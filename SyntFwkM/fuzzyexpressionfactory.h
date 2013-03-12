1d7432f8b1169105c6aeefe572dc9c8071c0daa2
Bruno-Muller <muller.bru@gmail.com>
2013-03-12 09:22:14 +0100

    lool

--- SyntFwkM/fuzzyexpressionfactory.h
+++ SyntFwkM/fuzzyexpressionfactory.h
@@ -10,6 +10,8 @@
 
 #include "expression.h"
 #include "expressionfactory.h"
+#include "is.h"
+#include "unaryshadowexpression.h"
 
 namespace fuzzy {
     
@@ -20,7 +22,19 @@ namespace fuzzy {
         virtual core::Expression<T>* NewOr(const core::Expression<T>* l, const core::Expression<T>* r);
         virtual core::Expression<T>* NewThen(const core::Expression<T>* l, const core::Expression<T>* r);
         virtual core::Expression<T>* NewAgg(const core::Expression<T>* l, const core::Expression<T>* r);
+        virtual core::Expression<T>* NewDefuzz(const core::Expression<T>* l, const core::Expression<T>* r);
+        virtual core::Expression<T>* NewNot(const core::Expression<T>* o);
+        virtual core::Expression<T>* NewIs(const core::Is<T>* is, const core::Expression<T>* o);
+        
+        //virtual void ChangeAnd(const And<T>* o);
+        //virtual void ChangeOr(const Or<T>* o);
+        //virtual void ChangeThen(const Then<T>* o);
+        //virtual void ChangeAgg(const Agg<T>* o);
+        //virtual void ChangeDefuzz(const Defuzz<T>* o);
+        //virtual void ChangeNot(const Not<T>* o);
+        //virtual void ChangeIs(const Is<T>* o);
     private:
+        //
        
     };
 }
