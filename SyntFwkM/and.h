16df5133fcb977c9d288beb7dd83d4ab3f32e6d4
CHEVALIER <s.chevaleir@iariss.fr>
2013-03-13 11:08:24 +0100

    new : mamdanidefuzz; modif : and, or, then

--- SyntFwkM/and.h
+++ SyntFwkM/and.h
@@ -1,6 +1,6 @@
 /* 
  * File:   and.h
- * Author: Simon
+ * Author: CHEVALIER Simon 2A-IR ENSISA <simon.cvr@gmail.com>
  *
  * Created on 13 mars 2013, 10:16
  */
---
+++ SyntFwkM/mamdanidefuzz.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   mamdanidefuzz.h
+ * Author: CHEVALIER Simon 2A-IR ENSISA <simon.cvr@gmail.com>
+ *
+ * Created on 13 mars 2013, 10:58
+ */
+
+#ifndef MAMDANIDEFUZZ_H
+#define	MAMDANIDEFUZZ_H
+
+#include "binaryexpression.h"
+
+namespace fuzzy
+{
+    template <class T>
+    class Mamdanidefuzz : public BinaryExpression<T>
+    {
+        
+    };
+}
+
+
+
+#endif	/* MAMDANIDEFUZZ_H */
+
