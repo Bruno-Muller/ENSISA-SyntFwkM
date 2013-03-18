86d83a497c2f29d065212ca519ad9a051ee48039
astazou <simon.cvr@gmail.com>
2013-03-18 08:39:18 +0100

    new : istrapeze

---
+++ SyntFwkM/istrapeze.h
@@ -0,0 +1,44 @@
+/* 
+ * File:   istrapeze.h
+ * Author: CHEVALIER Simon 2A-IR ENSISA <simon.cvr@gmail.com>
+ *
+ * Created on 18 mars 2013, 08:29
+ */
+
+#ifndef ISTRAPEZE_H
+#define	ISTRAPEZE_H
+
+#include "expression.h"
+#include "is.h"
+#include "istriangle.h"
+
+namespace fuzzy
+{
+    template <class T>
+    class IsTrapeze : public fuzzy::Is<T>
+    {
+        public :
+            IsTrapeze(const T& min, const T& mid, const T& max, const T& lenght);
+            virtual T evaluate(const core::Expression<T>* o) const;
+        private :
+            T m_min, m_mid, m_max, m_lenght;
+    };
+    
+    template <class T>
+    IsTrapeze<T>::IsTrapeze(const T& min, const T& mid, const T& max, const T& lenght)
+    {
+        m_min(min);
+        m_mid(mid);
+        m_max(max);
+        m_lenght(lenght);
+    }
+    
+    template <T>
+    T IsTriangle<T>::evaluate(const core::Expression<T>* o) const
+    {
+        return o->evaluate();
+    }
+}
+
+#endif	/* ISTRAPEZE_H */
+
