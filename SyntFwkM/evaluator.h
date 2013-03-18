0ac825f8dae6ab4be35b34b46be89f906eb25f2c
astazou <simon.cvr@gmail.com>
2013-03-18 08:39:08 +0100

    new : evaluator (not finish)

---
+++ SyntFwkM/evaluator.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   evaluator.h
+ * Author: CHEVALIER Simon 2A-IR ENSISA <simon.cvr@gmail.com>
+ *
+ * Created on 18 mars 2013, 08:26
+ */
+
+#ifndef EVALUATOR_H
+#define	EVALUATOR_H
+
+
+
+
+#endif	/* EVALUATOR_H */
+
