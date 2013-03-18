fe63edf2e481e6a772106f4077a84d3251b53f16
astazou <simon.cvr@gmail.com>
2013-03-18 09:04:46 +0100

    new : istrapezeright

---
+++ SyntFwkM/istrapezeright.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   istrapezeright.h
+ * Author: CHEVALIER Simon 2A-IR ENSISA <simon.cvr@gmail.com>
+ *
+ * Created on 18 mars 2013, 08:57
+ */
+
+#ifndef ISTRAPEZERIGHT_H
+#define	ISTRAPEZERIGHT_H
+
+#include "expression.h"
+#include "is.h"
+
+namespace fuzzy
+{
+    template <class T>
+    class IsTrapezeLeft : public fuzzy::Is<T>
+    {
+        public :
+            IsTrapezeLeft(const T& min, const T& max);
+            virtual T evaluate(const core::Expression<T>* o) const;
+        private :
+            T m_min, m_max;
+    };
+    
+    template <class T>
+    IsTrapezeLeft<T>::IsTrapezeLeft(const T& max, const T& min)
+    {
+        m_max(max);
+        m_min(min);        
+    }
+    
+    template <T>
+    T IsTrapezeLeft<T>::evaluate(const core::Expression<T>* o) const
+    {
+        return o->evaluate();
+    }
+}
+
+#endif	/* ISTRAPEZERIGHT_H */
+
